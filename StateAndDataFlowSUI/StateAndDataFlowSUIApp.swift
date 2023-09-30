f68f3b2d231bedac7fd638d06cc7047b59c2677b
Kirill <133357229+kirillussr@users.noreply.github.com>
2023-09-30 13:29:57 +0300

    Initial Commit

---
+++ StateAndDataFlowSUI/StateAndDataFlowSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateAndDataFlowSUIApp.swift
+//  StateAndDataFlowSUI
+//
+//  Created by Кирилл on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateAndDataFlowSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
